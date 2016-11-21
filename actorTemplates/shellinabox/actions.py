46855249146f2ec7095acc4d8caf99133097eb38
Christophe de Carvalho Pereira Martins <christophe.dcpm@gmail.com>
2016-11-21 13:25:07 +0400

    make sure binDir exists before trying to copy thing inside

--- actorTemplates/shellinabox/actions.py
+++ actorTemplates/shellinabox/actions.py
@@ -9,6 +9,7 @@ def install(job):
     def build_func(cuisine):
         cuisine.package.install('shellinabox')
         bin_path = cuisine.bash.cmdGetPath('shellinaboxd')
+        cuisine.core.dir_ensure('$binDir')
         if bin_path != j.sal.fs.pathClean(cuisine.core.args_replace("$binDir/shellinaboxd")):
             cuisine.core.file_copy(bin_path, "$binDir/shellinaboxd", overwrite=True)
 
