0115462bf8d4877862bd5dbf3705d11cca94e041
Christophe de Carvalho Pereira Martins <christophe.dcpm@gmail.com>
2016-11-29 09:46:12 +0400

    make sure we replace link with actual file

--- actorTemplates/cockpit/actions.py
+++ actorTemplates/cockpit/actions.py
@@ -9,6 +9,14 @@ def install(job):
     def build_func(cuisine):
         cuisine.solutions.cockpit.install(start=False)
 
+        # replace symbolic link with actual file
+        directories = [cuisine.core.args_replace('$appDir/ays_api/api_server'), cuisine.core.args_replace('$appDir/ays_api/ays_api')]
+        for directory in directories:
+            links = cuisine.core.fs_find(directory, type='l')
+            for link in links:
+                _, dest, _ = cuisine.core.run('readlink {}'.format(link), showout=False)
+                cuisine.core.run('rm {link}; cp -rv {dest} {link}'.format(link=link, dest=dest))
+
         js_script = r"""
         from JumpScale import j
         paths = []
