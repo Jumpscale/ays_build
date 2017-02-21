d6e73f9452b30c96e562350d26d2b4783bd59c9b
Reem Khamis <khamisr@codescalers.com>
2017-02-21 15:33:23 +0000

    Fixes for building on 8.1.1

--- actorTemplates/apache2/actions.py
+++ actorTemplates/apache2/actions.py
@@ -7,7 +7,7 @@ def install(job):
     from JumpScale.baselib.atyourservice81.AtYourServiceBuild import build
 
     def build_func(cuisine):
-        cuisine.development.php.build()
-        cuisine.development.php.install()
+        cuisine.apps.apache2.build()
+        cuisine.apps.apache2.install()
 
     build(job.service, build_func)
