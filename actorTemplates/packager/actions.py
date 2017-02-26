5fd1c89f04fcc0a6dca4ccac1b7f715fccace732
Reem Khamis <khamisr@codescalers.com>
2017-02-26 08:06:58 +0000

    Packager to go over all libs and sandbox them

--- actorTemplates/packager/actions.py
+++ actorTemplates/packager/actions.py
@@ -8,8 +8,28 @@ def install(job):
 
     def build_func(cuisine):
         service = job.service
-
         cuisine.tools.sandbox.cleanup()
+
+
+        js_script = r"""
+        from JumpScale import j
+        paths = []
+        paths.append("/usr/lib/python3/dist-packages")
+        paths.append("/usr/lib/python3.5/")
+        paths.append("/usr/local/lib/python3.5/dist-packages")
+        base_dir = j.tools.cuisine.local.core.dir_paths['base']
+        dest = j.sal.fs.joinPaths(base_dir, 'lib')
+        excludeFileRegex = ["-tk/", "/lib2to3", "-34m-", ".egg-info", "lsb_release"]
+        excludeDirRegex = ["/JumpScale", "\.dist-info", "config-x86_64-linux-gnu", "pygtk"]
+        for path in paths:
+            j.tools.sandboxer.copyTo(path, dest, excludeFileRegex=excludeFileRegex, excludeDirRegex=excludeDirRegex)
+        j.tools.sandboxer.copyTo('/usr/local/bin/', '%s/bin/' % base_dir, excludeFileRegex=excludeFileRegex, excludeDirRegex=excludeDirRegex)
+        if not j.sal.fs.exists("%s/bin/python" % base_dir):
+            j.sal.fs.symlink("%s/bin/python3" % base_dir, "%s/bin/python3.5" % base_dir, overwriteTarget=True)
+        j.tools.sandboxer.sandboxLibs("%s/lib" % base_dir, recursive=True)
+        j.tools.sandboxer.sandboxLibs("%s/bin" % base_dir, recursive=True)
+        """
+        cuisine.core.execute_jumpscript(js_script)
 
         sshkey = service.producers['sshkey'][0]
         cuisine.core.file_write("/root/.ssh/store_rsa", sshkey.model.data.keyPriv)
